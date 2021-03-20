0331573fd28149fd8aa1c28dfa854de3e8d8e041
caner demirci <caner.demirci@outlook.com>
2021-03-20 19:16:45 +0300

    commit

---
+++ Shared/Models/CountryDTO.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Shared.Models
+{
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+
+        public virtual IList<HotelDTO> Hotels { get; set; }
+    }
+
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country Name Is Too Long")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short Country Name Is Too Long")]
+        public string ShortName { get; set; }
+    }
+}
---
+++ Shared/Models/HotelDTO.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Shared.Models
+{
+    public class CreateHotelDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 250, ErrorMessage = "Hotel address is too long")]
+        public string Address { get; set; }
+        
+        [Required]
+        [Range(1, 5)]
+        public double Rating { get; set; }
+        
+        [Required]
+        public int CountryId { get; set; }
+    }
+
+    public class HotelDTO : CreateHotelDTO
+    {
+        public int Id { get; set; }
+        public CountryDTO Country { get; set; }
+    }
+}
